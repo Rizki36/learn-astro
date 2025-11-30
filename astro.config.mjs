c38d9ff298e8d57d459904f592d0150c6eb5d17e
rizki36 <39044004+Rizki36@users.noreply.github.com>
2025-11-30 22:56:25 +0700

    feat: update site URL configuration in astro.config.mjs and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
+import { SITE_URL } from "./src/utils/schema";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://www.devfitra.com", // Replace with your actual domain when it's ready
+	site: SITE_URL, // Replace with your actual domain when it's ready
 	integrations: [
 		sitemap({
 			// Configuration options for sitemap
-			filter: (page) => page !== "https://www.devfitra.com/admin", // exclude admin pages
+			filter: (page) => page !== `${SITE_URL}/admin`, // exclude admin pages
 			changefreq: "weekly",
 			priority: 0.7,
 			lastmod: new Date(),
