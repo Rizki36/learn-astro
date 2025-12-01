eafdf103376a9e1b71b2229362a09fca19951a8c
rizki36 <39044004+Rizki36@users.noreply.github.com>
2025-12-01 22:33:42 +0700

    chore: add @astrojs/vercel dependency to package.json

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,26 @@ import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import { SITE_URL } from "./src/utils/schema";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	site: SITE_URL, // Replace with your actual domain when it's ready
-	output: "server", // Enable server mode for API routes
-	integrations: [
-		react(),
-		sitemap({
-			// Configuration options for sitemap
-			filter: (page) => page !== `${SITE_URL}/admin`, // exclude admin pages
-			changefreq: "weekly",
-			priority: 0.7,
-			lastmod: new Date(),
-		}),
+  // Replace with your actual domain when it's ready
+  site: SITE_URL,
+
+  // Enable server mode for API routes
+  output: "server",
+
+  integrations: [
+      react(),
+      sitemap({
+          // Configuration options for sitemap
+          filter: (page) => page !== `${SITE_URL}/admin`, // exclude admin pages
+          changefreq: "weekly",
+          priority: 0.7,
+          lastmod: new Date(),
+      }),
 	],
+
+  adapter: vercel(),
 });
